3358d28a46078c77cfb446560e9d9e0d31bc2c15
renaidn <am10567@nyu.edu>
2021-06-21 17:38:53 +0600

    import

---
+++ js/music-list.js
@@ -0,0 +1,26 @@
+let allMusic = [
+  {
+    name: "BMO (feat. Doja Cat) - Remix",
+    artist: "Ari Lennox, Doja Cat",
+    img: "music-1", 
+    src: "music-1"
+  },
+  {
+    name: "Can't Decide",
+    artist: "Amine",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Crush",
+    artist: "Duckwrth",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Cityscape",
+    artist: "Adina",
+    img: "music-4",
+    src: "music-4"
+  },
+];
